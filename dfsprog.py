ae11bff5f4d470e6f964c8a0d395a4dfb5a61468
Akshata Sangwai <akshatasangwai1234@gmail.com>
2022-04-10 16:04:03 +0530

    dfs done

---
+++ dfsprog.py
@@ -0,0 +1,64 @@
+class Graph:
+
+    def __init__(self,n):
+        self.nodes = {}
+        for i in range(n):
+            self.nodes[i] = []
+    
+    def addNode(self,a,b):
+        self.nodes[a].append(b)
+    
+def DFS(graph,start,n):
+    status = [1]*n
+
+    stack = []
+
+    status[start] = 2
+    stack.append(0)
+    while queue:
+        element = queue.pop()
+
+        print(element)
+
+        neighbours = graph.nodes[element]
+
+        for neighbour in neighbours:
+            if status[neighbour] == 1:
+                status[neighbour] = 2
+                stack.append(neighbour)
+        print(stack)
+        status[element] = 3
+
+n = int(input("Enter the number of nodes : "))
+g = Graph(n)
+print("Enter -1,-1 to stop : ")
+while True:
+    inp = input().split(sep=",")
+    a,b = int(inp[0]),int(inp[1])
+    if a==-1 and b==-1:
+        break
+    g.addNode(a,b)
+
+print(g.nodes)
+DFS(g,0,n)
+
+# 0,1
+# 0,2
+# 0,3
+# 1,4
+# 2,1
+# 2,6
+# 3,2
+# 3,6
+# 4,2
+# 4,5
+# 5,2
+# 6,5
+# 7,4
+# 5,7
+# 6,5
+# 6,7
+# 6,8
+# 7,8
+# 8,5
+# -1,-1
